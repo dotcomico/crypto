85042a8969205311d8740fb3b2e88e7a792d9ff5
dotan <dotcompanyofficial@gmail.com>
2025-12-12 14:45:02 +0200

    Refactor coin card rendering in main.js for improved structure and functionality; simplify index.html by removing redundant markup

--- js/main.js
+++ js/main.js
@@ -30,7 +30,6 @@ $(() => {
       addCoinCard(element, index);
     });
   }
-
   function getCryptoCoins() {
     // קבלת נתונים Api
     fetch(API_COINS)
@@ -41,115 +40,159 @@ $(() => {
       })
       .catch((err) => alert(err));
   }
-
+  // הוספת כרטיס מטבע לקונטיינר
   function addCoinCard(coin, index) {
-    let Card = $("<div>").addClass("col-xl-3 col-lg-4 col-md-6 col-sm-12");
-    let inlineCard = $("<div>").addClass("card coin-card");
-    let cardBody = $("<div>").addClass("card-body");
-    let flex = $("<div>").addClass(
+    const card = createCoinCard(coin, index);
+    const inlineCard = card.find(".coin-card");
+    const moreInfoBtn = inlineCard.find(".btn-more-info");
+
+    handleMoreInfoToggle(coin, inlineCard, moreInfoBtn);
+
+    $coinsContainer.append(card);
+  }
+
+  //יצירת מבנה כרטיס
+  function createCoinCard(coin, index) {
+    const card = $("<div>").addClass("col-xl-3 col-lg-4 col-md-6 col-sm-12");
+    const inlineCard = $("<div>").addClass("card coin-card");
+    const cardBody = $("<div>").addClass("card-body");
+
+    const flex = createCardLayout(coin, index);
+
+    cardBody.append(flex);
+    inlineCard.append(cardBody);
+    card.append(inlineCard);
+
+    return card;
+  }
+
+  //יצירת לייאאוט פנימי לכרטיס
+  function createCardLayout(coin, index) {
+    const flex = $("<div>").addClass(
       "d-flex justify-content-between align-items-start"
     );
+    const leftDiv = createLeftSection(coin);
+    const switchDiv = createSwitchSection(index);
 
-    //  צד שמאל #####
-    let leftDiv = $("<div>");
+    flex.append(leftDiv, switchDiv);
+    return flex;
+  }
+
+  //יצירת צד שמאל
+  function createLeftSection(coin) {
+    const leftDiv = $("<div>");
     leftDiv.append(`<h5 class="card-title">${coin.symbol}</h5>`);
     leftDiv.append(`<p class="card-text">${coin.name}</p>`);
 
-    // כפתור more info
-    let moreInfoBtn = $("<button>")
-      .addClass("btn-more-info btn btn-primary btn-sm rounded-pill")
-      .attr({
-        type: "button",
-      })
-      .html(`<i class="bi bi-info-circle-fill"></i> More info`);
+    const moreInfoBtn = createMoreInfoButton();
     leftDiv.append(moreInfoBtn);
 
-    //  צד ימין ####
-    let switchDiv = $("<div>").addClass("form-check form-switch");
-    let switchInput = $("<input>")
+    return leftDiv;
+  }
+
+  // יצירת כפתור More Info
+  function createMoreInfoButton() {
+    return $("<button>")
+      .addClass("btn-more-info btn btn-primary btn-sm rounded-pill")
+      .attr({ type: "button" })
+      .html(`<i class="bi bi-info-circle-fill"></i> More info`);
+  }
+
+  //יצירת צד ימין - Switch
+  function createSwitchSection(index) {
+    const switchDiv = $("<div>").addClass("form-check form-switch");
+    const switchInput = $("<input>")
       .addClass("form-check-input switch")
       .attr({
         type: "checkbox",
         role: "switch",
         id: `switchCheckDefault-${index}`,
       });
+
     switchDiv.append(switchInput);
+    return switchDiv;
+  }
 
-    // חיבור שני הצדדים
-    flex.append(leftDiv);
-    flex.append(switchDiv);
-
-    // הוספה ל־card
-    cardBody.append(flex);
-    inlineCard.append(cardBody);
-    Card.append(inlineCard);
-    $coinsContainer.append(Card);
-
-    // משתנה לעקוב אחרי מצב הצגה
+  //טיפול באירוע לחיצה
+  function handleMoreInfoToggle(coin, inlineCard, moreInfoBtn) {
     let isInfoVisible = false;
     let collapse = null;
 
-    //---פעולות בעת לחיצה על כפתור---
-    moreInfoBtn.on("click", function () {
-      // אם מידע המטבע כבר מוצג - תסגור נראות
-      if (isInfoVisible && collapse) {
-        collapse.slideUp(300, function () {
-          collapse.remove();
-          collapse = null;
-        });
+    moreInfoBtn.on("click", () => {
+      if (isInfoVisible) {
+        closeInfo(collapse, moreInfoBtn);
         isInfoVisible = false;
-        moreInfoBtn.html(`<img class="bi bi-info-circle-fill"></i> More info`);
-        return;
-      }
-      // אם מידע המטבע לא מוצג
-      const cachedCoinData = coinsManager.getCoinDataBySymbol(coin.symbol);
-      if (cachedCoinData && cachedCoinData.isUpToDate()) {
-        // בדוק תקינות נתונים שמורים
-        //נתונים תקינים - הצג מחדש
-        collapse = $("<div>").css("display", "none");
-        let collapseBody = creatExtendedData(cachedCoinData);
-        collapse.append(collapseBody);
-        inlineCard.append(collapse);
-        collapse.slideDown(300);
-        isInfoVisible = true;
-        moreInfoBtn.html(`<i class="bi bi-x-circle-fill"></i> Close`);
-        console.log(coin.symbol + "תונים תקינים - מציג מחדש");
+        collapse = null;
       } else {
-        // נתונים לא תקינים - צור מחדש והצג
-        console.log(coin.symbol + " נתונים לא תקינים - יוצר מחדש ומציג");
-        // שנה נראות כפתור לטעינת נתונים
-        moreInfoBtn.html(
-          `<img src='Image/hourglass.gif' style="max-width: 20px;"></img> Loading...`
-        );
-        //קבל נתונים
-        fetch(API_COINS_DATA + coin.id)
-          .then((res) => res.json())
-          .then((data) => {
-            const newCoinData = new CoinData(
-              coin.name,
-              coin.symbol,
-              data.image.small,
-              data.market_data.current_price.usd,
-              data.market_data.current_price.eur,
-              data.market_data.current_price.ils
-            );
-            coinsManager.saveCoinData(newCoinData);
-            collapse = $("<div>").css("display", "none");
-            let collapseBody = creatExtendedData(newCoinData);
-            collapse.append(collapseBody);
-            inlineCard.append(collapse);
-            collapse.slideDown(300);
-            isInfoVisible = true;
-            moreInfoBtn.html(`<i class="bi bi-x-circle-fill"></i> Close`);
-          })
-          .catch((err) => {
-            alert('"More info" API error: ' + err);
-            moreInfoBtn.html(
-              `<i class="bi bi-info-circle-fill"></i> More info`
-            );
-          });
+        collapse = showInfo(coin, inlineCard, moreInfoBtn);
+        isInfoVisible = true;
       }
     });
+  }
+
+  // סגירת מידע נוסף
+  function closeInfo(collapse, moreInfoBtn) {
+    collapse.slideUp(300, function () {
+      collapse.remove();
+    });
+    moreInfoBtn.html(`<i class="bi bi-info-circle-fill"></i> More info`);
+  }
+
+  //הצגת מידע נוסף
+  function showInfo(coin, inlineCard, moreInfoBtn) {
+    const cachedCoinData = coinsManager.getCoinDataBySymbol(coin.symbol);
+
+    if (cachedCoinData && cachedCoinData.isUpToDate()) {
+      console.log(cachedCoinData.symbol + "נתונים תקינים, מציג מחדש");
+      moreInfoBtn.html(`<i class="bi bi-x-circle-fill"></i> Close`);
+     return createAndShowCollapse(cachedCoinData, inlineCard);
+    } else {
+      console.log(coin.symbol + "נתונים לא תקינים, שולף מחדש");
+      return fetchAndDisplayData(coin, inlineCard, moreInfoBtn);
+    }
+  }
+
+  //שליפה ושימוש בנתונים עדכניים
+   function fetchAndDisplayData(coin, inlineCard, moreInfoBtn) {
+    setLoadingState(moreInfoBtn);
+
+    const collapse = $("<div>").css("display", "none");
+    inlineCard.append(collapse);
+
+    fetch(API_COINS_DATA + coin.id)
+      .then((res) => res.json())
+      .then((data) => {
+        const newCoinData = new CoinData(
+          coin.name,
+          coin.symbol,
+          data.image.small,
+          data.market_data.current_price.usd,
+          data.market_data.current_price.eur,
+          data.market_data.current_price.ils
+        );
+        coinsManager.saveCoinData(newCoinData);
+
+        const collapseBody = creatExtendedData(newCoinData);
+        collapse.append(collapseBody);
+        collapse.slideDown(300);
+        moreInfoBtn.html(`<i class="bi bi-x-circle-fill"></i> Close`);
+      })
+      .catch((err) => handleFetchError(err, moreInfoBtn));
+
+    return collapse;
+  }
+
+  //מצב טעינה
+  function setLoadingState(moreInfoBtn) {
+    moreInfoBtn.html(
+      `<img src='Image/hourglass.gif' style="max-width: 20px;"> Loading...`
+    );
+  }
+
+  //טיפול בשגיאות
+  function handleFetchError(err, moreInfoBtn) {
+    alert('"More info" API error: ' + err);
+    moreInfoBtn.html(`<i class="bi bi-info-circle-fill"></i> More info`);
   }
 
   function creatExtendedData({
@@ -183,4 +226,12 @@ $(() => {
                     `);
     return extendedData;
   }
+  function createAndShowCollapse(coinData, inlineCard) {
+    const collapse = $("<div>").css("display", "none");
+    const collapseBody = creatExtendedData(coinData);
+    collapse.append(collapseBody);
+    inlineCard.append(collapse);
+    collapse.slideDown(300);
+    return collapse;
+}
 });
