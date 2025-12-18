5c22cef64f77fa783ec3131863a4e103c74b558c
dotan <dotcompanyofficial@gmail.com>
2025-12-18 23:53:32 +0200

    ככ

--- js/api/CoinApiService.js
+++ js/api/CoinApiService.js
@@ -2,14 +2,14 @@
 //Handles asynchronous REST API calls
 
 const API_COINS =
-  "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=30&page=1";
+  "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1";
 const API_COINS_DATA = "https://api.coingecko.com/api/v3/coins/"; // + COIN ID
 
 // קבלת נתונים Api
 export function getCryptoCoins() {
   return fetch(API_COINS)
     .then((res) => res.json())
-    .then((data) => data.slice(0, 30));
+    .then((data) => data.slice(0, 100));
 }
 
 // קבלת נתונים מורחבים למטבע בודד
